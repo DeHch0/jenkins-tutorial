9c010dfcbc6c301c70d09c679fa5e3bb2d4f6761
DeHch0 <denisrumenov98@gmail.com>
2025-12-27 14:31:01 +0200

    efef

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React version 24
+          Learn React version 25
         </a>
       </header>
     </div>
